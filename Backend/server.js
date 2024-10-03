1a8336e9ff18422a3edb9fb3e875f7746928a009
Komal Navale <120366966+komal1157@users.noreply.github.com>
2024-10-03 16:55:33 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from "express";
 import connectDB from './db/index.js';
 import UserRoute from "./routes/UserRoute.js";
@@ -6,28 +5,38 @@ import authRoutes from "./routes/authRoutes.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import CreateUser from "./controllers/NewUser.js";
-import  authController from "./controllers/authController.js";
+import  authController, { register } from "./controllers/authController.js";
 import axios from "axios";
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
 
-app.use("/NewUser", UserRoute);
+app.use("/NewUser",  UserRoute);
 
-app.use("/authController", authRoutes);
+app.use("/authController",  authRoutes);
 
 connectDB();
 
 app.post('/User', CreateUser);//post request to store data
 
+app.post('/User',register);
 
 // Start server
 app.listen(PORT, () => {
